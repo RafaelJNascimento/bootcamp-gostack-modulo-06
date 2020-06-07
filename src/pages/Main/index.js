2b1917a9a5553a81f4a49f90107c64eb52f84b89
Rafael José do Nascimento <rafaeljosev10@gmail.com>
2020-06-07 03:37:45 -0300

    10 - Acessando API do Github 'axios melhorado'

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function App({ navigation }) {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#FFF" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    await api
+      .get(`/users?q=${newUser}`)
+      .then((response) => {
+        const data = {
+          name: response.data.items.name,
+          login: response.data.items.login,
+          bio: response.data.items.bio,
+          avatar: response.data.items.avatar_url,
+        };
+
+        this.setState({
+          users: [...users, data],
+          newUser: '',
+        });
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={(text) => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
