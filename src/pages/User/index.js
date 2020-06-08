501dccdedc27eb7385aa64cafb406627853f00c8
Rafael José do Nascimento <rafaeljosev10@gmail.com>
2020-06-07 23:04:47 -0300

    17 - Listando favoritos

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
-import { View } from 'react-native';
 import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   static propTypes = {
@@ -19,12 +32,34 @@ export default class User extends Component {
     this.props.navigation.setOptions({ title: user.name });
     await api.get(`/users/${user.login}/starred`).then((response) => {
       this.setState({ stars: response.data });
-      console.log(response.data);
     });
   }
   render() {
     const { stars } = this.state;
+    const user = this.props.route.params.user;
 
-    return <View />;
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={(star) => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
   }
 }
